67546354b1191f2754e8d535412f73342eb9de3e
Rafael Kerr <rafael.kerr@gmail.com>
2024-08-19 11:28:37 -0300

    fix api config

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,6 +1,6 @@
 export default {
     FRONTEND_URL: 'https://platform.flourishfi.com',
-    BACKEND_API_URL: 'https://api.flourishsavings.com/api/v1',
+    BACKEND_API_URL: 'https://api.flourishfi.com/api/v1',
     FLOURISH_SDK_APP_VERSION: '1.0.0'
   };
   
--- src/config/config.staging.js
+++ src/config/config.staging.js
@@ -1,6 +1,6 @@
 export default {
     FRONTEND_URL: 'https://platform-stg.flourishfi.com',
-    BACKEND_API_URL: 'https://staging.flourishsavings.com/api/v1',
+    BACKEND_API_URL: 'https://api-stg.flourishfi.com/api/v1',
     FLOURISH_SDK_APP_VERSION: '1.0.0'
   };
   
