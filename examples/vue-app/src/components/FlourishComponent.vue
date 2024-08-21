6908f8dbaade60898592049c0133d96df8d5729f
Rafael Kerr <rafael.kerr@gmail.com>
2024-08-21 16:47:55 -0300

    Create Vue example app

---
+++ examples/vue-app/src/components/FlourishComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="flourish-container" ref="flourishContainer"></div>
+  </template>
+  
+  <script>
+  import { Flourish } from 'flourish-sdk-web';
+  
+  export default {
+    name: 'FlourishComponent',
+    props: {
+      token: {
+        type: String,
+        required: true
+      },
+      language: {
+        type: String,
+        default: 'en'
+      },
+      environment: {
+        type: String,
+        required: true
+      }
+    },
+    mounted() {
+      // Initialize the Flourish SDK and append the iframe to the container
+      const flourishApp = Flourish(this.token, this.language, this.environment, this.handleGenericEvent);
+      this.$refs.flourishContainer.appendChild(flourishApp);
+    },
+    methods: {
+      handleGenericEvent(data) {
+        console.log('Generic event received:', data);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .flourish-container {
+    width: 100%;
+    height: 100vh; /* Full viewport height */
+  }
+  </style>
+  
